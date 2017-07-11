0ee84fd38f5b569d23b7bb594a26568adeae0480
nik2402 <nikhil.nikhil68@gmail.com>
2017-07-12 01:20:32 +0200

    Adding the exercise 05 and 06

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -3,6 +3,20 @@ class Book
 attr_accessor :title
 
 def title
-	return @title.capitalize
+	splitArray = @title.split(" ")
+	neverCapitalize = ["a","an","the","and","in","of"]
+	alwaysCapitalize = "I"
+	i = 0
+	splitArray.each do |word|
+		if !(neverCapitalize.include? word.downcase) or i==0
+			splitArray[i] = word.capitalize
+		elsif alwaysCapitalize.eql? word.downcase
+			splitArray[i] = word.capitalize
+		end
+
+		i = i + 1
+	end
+
+	return splitArray.join(" ")
 end
 end
--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,36 @@
 class Timer
+	attr_accessor :seconds, :time_string
   #write your code here
+  def initialize
+  	@seconds = 0
+  end
+
+  def time_string
+
+  	sec = @seconds%60
+  	minutes = @seconds/60
+  	hours = minutes/60
+  	minutes = minutes - hours * 60 
+
+  	if minutes.to_s.length == 1
+  		minutes = "0" + minutes.to_s
+  	else
+  		minutes = minutes.to_s
+  	end
+
+	if hours.to_s.length == 1
+  		hours = "0" + hours.to_s
+  	else
+  		hours = hours.to_s
+  	end
+
+	if sec.to_s.length == 1
+  		sec = "0" + sec.to_s
+  	else
+  		sec = sec.to_s
+  	end
+  	puts  hours + ":" + minutes + ":" + sec
+  	return hours + ":" + minutes + ":" + sec
+  	
+  end
 end
